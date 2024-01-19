29ad2a21e8394f214a87a84628ae797924d3685d
XE.PAN <sswi@vip.qq.com>
2024-01-19 23:18:39 +0800

    .NET8.0+WPF+Blazor+FluentUI项目搭建，重启

--- GameScriptsPlayer/App.xaml.cs
+++ GameScriptsPlayer/App.xaml.cs
@@ -1,5 +1,6 @@
-﻿using System.Configuration;
-using System.Data;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.FluentUI.AspNetCore.Components;
+using Microsoft.FluentUI.AspNetCore.Components.DesignTokens;
 using System.Windows;
 
 namespace GameScriptsPlayer
@@ -9,6 +10,47 @@ namespace GameScriptsPlayer
     /// </summary>
     public partial class App : Application
     {
-    }
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            var serviceCollection = new ServiceCollection();
+            UseComponents(serviceCollection);  
+            Register(serviceCollection);
+            Resources.Add("services", serviceCollection.BuildServiceProvider());
+        }
 
+
+        /// <summary>
+        /// 注册组件
+        /// </summary>
+        /// <param name="services"></param>
+        private static void UseComponents( ServiceCollection services)
+        {
+            services.AddFluentUIComponents(); //注册FluentUI  
+            services.AddBlazorWebViewDeveloperTools(); //启用调试工具
+
+
+
+
+            services.AddWpfBlazorWebView();//启用bazlor组件，要放在最后
+        }
+
+
+        /// <summary>
+        /// 注入
+        /// </summary>
+        /// <param name="services"></param>
+        private static void Register(ServiceCollection services)
+        {
+            
+            services.AddTransient<BaseLayerLuminance>();
+       
+            
+
+            //注入
+
+
+             
+        }
+    }
 }
--- GameScriptsPlayer/MainWindow.xaml.cs
+++ GameScriptsPlayer/MainWindow.xaml.cs
@@ -1,24 +1,18 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using Wpf.Ui.Appearance;
+using Wpf.Ui.Controls;
 
 namespace GameScriptsPlayer
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : FluentWindow
     {
         public MainWindow()
         {
             InitializeComponent();
+
+            Loaded += (sender, args) => SystemThemeWatcher.Watch(this, WindowBackdropType.Mica, true, true);
         }
     }
 }
